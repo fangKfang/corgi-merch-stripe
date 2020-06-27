fa1339016f7df0b22ad917908221a827ce38a261
fangKfang <jalgena@gmail.com>
2020-06-27 14:58:07 -0700

    first commit

---
+++ client/src/api.js
@@ -0,0 +1,141 @@
+const base_url = "http://127.0.0.1:4242"
+
+const createPaymentIntent = options => {
+  console.log(options)
+  return window
+    .fetch(`${base_url}/create-payment-intent`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(options)
+    })
+    .then(res => {
+      if (res.status === 200) {
+        return res.json();
+      } else {
+        return null;
+      }
+    })
+    .then(data => {
+      if (!data || data.error) {
+        console.log("API error:", { data });
+        throw new Error("PaymentIntent API Error");
+      } else {
+        return data.client_secret;
+      }
+    });
+};
+
+const getProducts = options => {
+  return window
+    .fetch(`${base_url}/products`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+    .then(res => {
+      if (res.status === 200) {
+        return res.json();
+      } else {
+        return null;
+      }
+    })
+    .then(data => {
+      if (!data || data.error) {
+        console.log("API error:", { data });
+        throw Error("API Error");
+      } else {
+        return data;
+      }
+    });
+};
+
+const getProductDetails = options => {
+  return window
+    .fetch(`${base_url}/product-details`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+    .then(res => {
+      if (res.status === 200) {
+        return res.json();
+      } else {
+        return null;
+      }
+    })
+    .then(data => {
+      if (!data || data.error) {
+        console.log("API error:", { data });
+        throw Error("API Error");
+      } else {
+        return data;
+      }
+    });
+};
+
+
+
+const getPublicStripeKey = options => {
+  return window
+    .fetch(`${base_url}/public-key`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+    .then(res => {
+      if (res.status === 200) {
+        return res.json();
+      } else {
+        return null;
+      }
+    })
+    .then(data => {
+      if (!data || data.error) {
+        console.log("API error:", { data });
+        throw Error("API Error");
+      } else {
+        return data.publicKey;
+      }
+    });
+};
+
+const postReceiptData = options => {
+  return window
+    .fetch(`${base_url}/purchases`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(options)
+    })
+    .then(res => {
+      if (res.status === 200) {
+        return res.json();
+      } else {
+        return null;
+      }
+    })
+    .then(data => {
+      if (!data || data.error) {
+        console.log("API error:", { data });
+        throw new Error("Failed to Send Payment Metadata");
+      } else {
+        return {};
+      }
+    });
+};
+
+const api = {
+  createPaymentIntent,
+  getPublicStripeKey: getPublicStripeKey,
+  getProductDetails: getProductDetails,
+  getProducts: getProducts,
+  postReceiptData,
+};
+
+export default api;
